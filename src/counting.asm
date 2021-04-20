8ae709e392f0cf425cbc8100034f8bae35c71d18
Michael Purdy <michaeljosephpurdy@gmail.com>
2021-04-19 22:24:50 -0400

    Drawing some stuff to the screen

--- src/counting.asm
+++ src/counting.asm
@@ -1,41 +1,56 @@
 SECTION "Counting_variables", WRAM0
-TimerMS: DS 1
-TimerS: DS 1
-TimerM: DS 1
-TimerShouldCount: DS 1
+Timer_ms: DS 2
+Timer_s: DS 2
+Timer_m: DS 2
+Timer_goal: DS 2
+Timer_shouldCount: DS 1
 
 SECTION "CountingInit", ROM0
 Counting_init::
-	push af
 	xor a
-	ld [TimerMS], a
-	ld [TimerS], a
-	ld [TimerM], a
-	pop af
+	ld [Timer_ms], hl
+	ld [Timer_s], a
+	ld [Timer_m], a
+	ld a, 1 
+	ld [Timer_goal], a
+	ld a, 1
+	ld [Timer_shouldCount], a
 	ret
 
 SECTION "CountingUpdate", ROM0
 Counting_update::
-	ld a, [TimerMS]
+	ld a, [Timer_shouldCount]
+	sub a, 1
+	jr z, .done
+	ld a, [Timer_ms]
 	inc a
-	ld [TimerMS], a
+	ld [Timer_ms], a
 	cp 60
-	jr z, .countSeconds
+	jr z, .incrementSeconds
 	jr .stopCounting
-.countSeconds:
+.incrementSeconds:
 	xor a
-	ld [TimerMS], a
-	ld a, [TimerS]
+	ld [Timer_ms], a
+	ld a, [Timer_s]
 	inc a
-	ld [TimerS], a
+	ld [Timer_s], a
 	cp 60
-	jr z, .countMinutes
+	jr z, .incrementMinutes
 	jr .stopCounting
-.countMinutes:
+.incrementMinutes:
 	xor a
-	ld [TimerS], a
-	ld a, [TimerM]
+	ld [Timer_s], a
+	ld a, [Timer_m]
 	inc a
-	ld [TimerM], a
+	ld [Timer_m], a
+	ld l, a
+	; check to see if we have reached our goal
+	ld a, [Timer_goal]
+	sub a, l
+	jr z, .done
 .stopCounting:
   ret
+.done:
+	xor a
+	ld [Timer_shouldCount], a
+	ret
--- src/tomato.asm
+++ src/tomato.asm
@@ -4,25 +4,20 @@ TomatoY: DS 1
 
 SECTION "Tomato_rom", ROM0
 TomatoSprite:
-  dw `00000000
-  dw `00000000
-  dw `00002200
-  dw `00222222
-  dw `00222222
-  dw `00222222
-  dw `00022233
-  dw `00022233
-  dw `00000000
-  dw `00000000
-  dw `00000000
-  dw `22220000
-  dw `22220000
-  dw `22220000
-  dw `22220000
-  dw `22220000
+  DB $aa, $00, $01, $00, $80, $00, $01, $00
+  DB $80, $00, $01, $00, $80, $00, $55, $00
+  DB $aa, $00, $01, $00, $80, $00, $01, $00
+  DB $80, $00, $01, $00, $80, $00, $55, $00
+  DB $aa, $00, $01, $00, $80, $00, $01, $00
+  DB $80, $00, $01, $00, $80, $00, $55, $00
+  DB $aa, $00, $01, $00, $80, $00, $01, $00
+  DB $80, $00, $01, $00, $80, $00, $55, $00
 
 SECTION "Tomato_init", ROM0
 Tomato_oam1 EQU $FE00
+Tomato_oam2 EQU $FE04
+Tomato_oam3 EQU $FE08
+Tomato_oam4 EQU $FE0C
 Tomato_init::
   ld a, 20
   ld [TomatoX], a
@@ -35,24 +30,62 @@ Tomato_draw::
   ; we need to copy the data from ROM into VRAM
   ld hl, TomatoSprite ; label where the sprite data exists
   ; get the location _not_ the value
-  ld de, $8010 ; set the destination for the copy
-  ld b, 32 ; size counter - how much we'll copy
+  ld de, $8000 ; set the destination for the copy
+  ld b, 64 ; size counter - how much we'll copy
   ; it doesn't look like it is 16 bytes
   ; but the shorthand ` is decieving
 .loop
   ld a, [hli] ; grab a byte and increment
   ld [de], a ; copy it to the destination
-  inc e     ; increment the low-byte (e) of the destination
+  inc de     ; increment the low-byte (e) of the destination
   dec b      ; decrement the size counter
   jr nz, .loop ; if b isn't 0, keep working
 .setup_oam
+
   ; top left
   ld hl, Tomato_oam1
-  ld a, [TomatoX]
-  ld [hli], a
   ld a, [TomatoY]
   ld [hli], a
+  ld a, [TomatoX]
+  ld [hli], a
+  ld a, 0
+  ld [hli], a
+  xor a
+  ld [hli], a
+
+  ; top right
+  ld hl, Tomato_oam2
+  ld a, [TomatoY]
+  ld [hli], a
+  ld a, [TomatoX]
+  add a, 8
+  ld [hli], a
   ld a, 1
+  ld [hli], a
+  xor a
+  ld [hl], a
+
+  ; bot left 
+  ld hl, Tomato_oam3
+  ld a, [TomatoY]
+  add a, 8
+  ld [hli], a
+  ld a, [TomatoX]
+  ld [hli], a
+  ld a, 2
+  ld [hli], a
+  xor a
+  ld [hl], a
+
+  ; bot right
+  ld hl, Tomato_oam4
+  ld a, [TomatoY]
+  add a, 8
+  ld [hli], a
+  ld a, [TomatoX]
+  add a, 8
+  ld [hli], a
+  ld a, 3
   ld [hli], a
   xor a
   ld [hl], a
